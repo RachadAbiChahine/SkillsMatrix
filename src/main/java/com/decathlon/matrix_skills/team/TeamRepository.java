f4c51e9d5f3ee8ce079e9c8c6ee1936aac220700
Rachad Abi Chahine <rachad.abi-chahine@hotmail.fr>
2018-11-22 01:48:38 +0100

    adds @Transactional on deleteByTeamName
    Spring only marks the default methods of the CrudRepository interface as transactional.

--- src/main/java/com/decathlon/matrix_skills/team/TeamRepository.java
+++ src/main/java/com/decathlon/matrix_skills/team/TeamRepository.java
@@ -4,12 +4,14 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Repository
 public interface TeamRepository extends JpaRepository<Team, Long> {
 
+    @Transactional
     List<Team> deleteByTeamName(String teamName);
 
     Team findFirstByTeamNameIgnoreCase(String teamName);
